82e81d4b9fe4ed108efac876157c35e5dd7dac91
Stan <s_northman@hotmail.com>
2023-04-04 12:49:48 +0200

    add test data

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,6 +1,19 @@
+from pathlib import Path
+
 import pytest
 
-from reconai.data.Volume import Volume
+from reconai.data.data import gather_data
+from reconai.data.Batcher import Batcher
+
 
 def test_volume():
+    data = gather_data(Path('input'))
+    data_error = Batcher(data).get_blacklist()
+    data = list(filter(lambda a: a.study_id not in data_error, data))
+    data_n = len(data)
 
+    batcher = Batcher(data)
+    for item in batcher.generate():
+        # TODO: check this with input yaml config file rather than constant val
+        assert item.shape == (1, 15, 256, 256)
+    pass
