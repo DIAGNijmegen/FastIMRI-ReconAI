7e8fdf9c99c04d831295cbd631fc944fcabb3ebc
Stan <s_northman@hotmail.com>
2023-04-12 16:52:51 +0200

    implemented zoom, better tests

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,16 +1,17 @@
 import json
 from pathlib import Path
-
-from reconai.data.data import gather_data
-from reconai.data.Batcher import Batcher as OldBatcher
+import matplotlib.pyplot as plt
 
 import pytest
 import numpy as np
 
-from reconai.data.batcher1 import Batcher
+from reconai.data.batcher1 import Batcher, zoom, rotate, crop_or_pad, flip_ud_lr, normalize
 from reconai.data.dataloader import DataLoader
 from reconai.data.sequence import SequenceCollection
 from reconai.data.sequencer import Sequencer
+
+from reconai.data.data import gather_data
+from reconai.data.Batcher import Batcher as OldBatcher
 
 
 @pytest.fixture
@@ -56,10 +57,57 @@ def test_generate_sequence(dataloader: DataLoader, sequences: SequenceCollection
     assert obj1 != seq.generate_sequences(**kwargs)
 
 
+def test_batcher_append_sequence(dataloader: DataLoader, sequences: SequenceCollection):
+    batcher = Batcher(dataloader)
+    batcher.append_sequence(next(sequences.items()))
+
+    show = False
+    expected_output = './output/test_batcher_append_sequence_expected_'
+
+    def imshow(img, title, show=False, save=False):
+        fig = plt.figure()
+        plt.imshow(img, cmap='Greys_r')
+        fig.suptitle(title)
+        if save:
+            np.save(expected_output + title, img)
+        if show:
+            fig.show()
+
+    sequence_images = next(batcher.items())[3]
+    mean = sequence_images.mean()
+    imshow(sequence_images, "original", show=show)
+
+    z = 2
+    sequence_images_zoom = zoom(sequence_images, z)
+    assert all(sequence_images.shape[i] * z == sequence_images_zoom.shape[i] for i in range(2))
+    assert np.isclose(mean, sequence_images_zoom.mean(), atol=2)
+    imshow(sequence_images_zoom, "zoom", show=show)
+
+    sequence_images_rotate = rotate(sequence_images, 45)
+    sequence_images_rotate_path = Path(expected_output + 'rotate').with_suffix('.npy')
+    imshow(sequence_images_rotate, "rotate", show=show)
+    assert np.allclose(np.load(str(sequence_images_rotate_path)), sequence_images_rotate)
+
+    for x in [128, 256, 512]:
+        for y in [128, 256, 512]:
+            sequence_images_crop = crop_or_pad(sequence_images, (x, y))
+            assert sequence_images_crop.shape == (x, y)
+
+    for f in [[''], ['lr'], ['ud'], ['lr', 'ud']]:
+        sequence_images_flip = flip_ud_lr(sequence_images, *f)
+        sequence_images_flip_path = Path(expected_output + 'flip' + ''.join(f)).with_suffix('.npy')
+        imshow(sequence_images_flip, 'flip' + ''.join(f), show=show)
+        assert np.isclose(mean, sequence_images_flip.mean())
+        assert np.allclose(np.load(str(sequence_images_flip_path)), sequence_images_flip)
+
+    sequence_images_norm = normalize(sequence_images, 2000)
+    assert np.isclose(mean / 2000, sequence_images_norm.mean())
+
+
 def test_batcher(dataloader: DataLoader, sequences: SequenceCollection):
     batcher = Batcher(dataloader)
     for s in sequences.items():
-        batcher.append_sequence(s)
+        batcher.append_sequence(s, norm=2, flip='lr', zoom_factor=1.1, rotate_deg=5)
 
     max_folds = 4
 
@@ -80,6 +128,7 @@ def test_batcher(dataloader: DataLoader, sequences: SequenceCollection):
         for v in va:
             assert all(not np.array_equal(v, t) for t in tr)
 
+    # test for seed
     batcher.shuffle(10)
     obj1 = next(batcher.items())
     batcher.shuffle(10)
